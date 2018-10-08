changed spherical model to have variable variance for mu posterior, fixed appropriate bounds

spherical/bound_check.py
@@ -3,32 +3,35 @@ from scipy.stats import norm, gamma, rv_discrete, beta
 from scipy.special import digamma
 from scipy.special import gamma as gammaf
 
+lgamma = lambda x: np.log(gammaf(x))
+
 """
-SPHERICAL COVARIANCE, i.e. isotropic
+DIAGONAL COVARIANCE
 
 Code to check analytical derivations of ELBO in Variational_Inference_in_DPGMM_derivation.pdf against Monte Carlo estimates 
 
 python bound_check.py
 
-mcusi@mit.edu, july 2018
+lbh@mit.edu, october 2018
 """
 
 np.random.seed(0)
 
-D=1
+D=3
 K=2
 
-nu = np.random.randn(K)
+nu = np.random.randn(K, D)
+omega = np.random.random([K]) + 1
 zeta = np.array([0.2, 0.8])
 
-a = np.ones(K)
-b = 1.5*np.ones(K)
+a = np.ones([K],dtype=np.float32)
+b = 1.5*np.ones([K],dtype=np.float32)
 
-lambda1 = np.ones(K)
-lambda2 = 2.*np.ones(K)
+lambda1 = np.ones(K,dtype=np.float32)
+lambda2 = 2.*np.ones(K,dtype=np.float32)
 
 alpha = 3.
-p_phi = beta(1, alpha)
+p_phi = beta(1., alpha)
 p_mu = norm
 p_tau = gamma(a=1., scale=1.)
 def log_p_z(phi, z):
@@ -37,18 +40,42 @@ def log_p_z(phi, z):
 def p_x(z, mu, tau): return norm(loc=mu[z], scale=np.sqrt(1./tau[z]))
 
 q_phi = beta(lambda1, lambda2)
-q_mu = norm(loc=nu)
+q_mu = norm(loc=nu, scale=1./np.sqrt(omega[:, None]))
 q_tau = gamma(a=a, scale=1./b) #tau is precision!
 q_z = rv_discrete(values=(range(K), zeta))
 
-x = 3.
+x = np.array([1., 2., 3.])
 
-N = 20001
+N = 200001
+
 print('Spherical Covariance Model')
+
+# ####### mu term in the ELBO ########
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+# Analytical
+# OLD:
+# bound = sum(-0.5*nu_k**2 for nu_k in nu)
+# NEW:
+bound = sum( #over k
+	-D/2. * (1./omega + np.log(omega) - 1)
+	- 0.5 * (nu**2).sum(axis=1)
+)
+print("Analytical mu term in ELBO:", bound)
+
+# Monte Carlo
+print('Monte Carlo estimate of mu term in ELBO:')
+np.random.seed()
+bounds = []
+for i in range(N):
+	mu = q_mu.rvs()
+	bounds.append(sum(sum(p_mu.logpdf(mu) - q_mu.logpdf(mu)))) #Sum over K for MC estimate
+	if i%5000 == 0: print(i, np.mean(bounds))
+
+
+
 # ####### phi term in the ELBO ########
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 # Analytical
-lgamma = lambda x: np.log(gammaf(x))
 bound = sum((lgamma(1. + alpha) - lgamma(alpha)
 			+ (alpha - 1.)*(digamma(l2_k) - digamma(l1_k + l2_k))
 			- lgamma(l1_k + l2_k) + lgamma(l1_k) + lgamma(l2_k)
@@ -67,27 +94,17 @@ for i in range(N):
 	bounds.append(sum(p_phi.logpdf(phi) - q_phi.logpdf(phi))) #Sum over K for MC estimate
 	if i%5000 == 0: print(i, np.mean(bounds))
 
-# ####### mu term in the ELBO ########
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-# Analytical
-bound = sum(-0.5*nu_k**2 for nu_k in nu)
-print("Analytical mu term in ELBO:", bound)
 
-# Monte Carlo
-print('Monte Carlo estimate of mu term in ELBO:')
-np.random.seed()
-bounds = []
-for i in range(N):
-	mu = q_mu.rvs()
-
-	bounds.append(sum(p_mu.logpdf(mu) - q_mu.logpdf(mu))) #Sum over K for MC estimate
-	if i%5000 == 0: print(i, np.mean(bounds))
 
 # ####### tau term in the ELBO ########
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 # Analytical
-bound = sum(lgamma(a_k) - (a_k-1.)*digamma(a_k) - np.log(b_k) + 1 - np.divide(a_k,b_k)
-			for (a_k, b_k) in zip(a, b))
+#OLD:
+# bound = sum(lgamma(a_k) - (a_k-1.)*digamma(a_k) - np.log(b_k) + 1 - np.divide(a_k,b_k)
+# 			for (a_k, b_k) in zip(a, b))
+#NEW:
+bound = sum(lgamma(a_k) - (a_k-1.)*digamma(a_k) - np.log(b_k) + a_k - np.divide(a_k,b_k)
+ 			for (a_k, b_k) in zip(a, b))
 print("Analytical tau term in ELBO:", bound)
 
 # Monte Carlo
@@ -99,6 +116,9 @@ for i in range(N):
 
 	bounds.append(sum(p_tau.logpdf(tau) - q_tau.logpdf(tau))) #Sum over K for MC estimate
 	if i%5000 == 0: print(i, np.mean(bounds))
+
+
+
 
 # ####### z term in the ELBO ########
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
@@ -121,14 +141,19 @@ for i in range(N):
 	bounds.append(log_p_z(phi, z) - q_z.logpmf(z)) #There's only a single datapoint, so no need for sum
 	if i%5000 == 0: print(i, np.mean(bounds))
 
+
+
+
 # ####### x term in the ELBO ########
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 # Analytical
-bound = sum(zeta_k * (
-			-D/2. * (np.log(2 * np.pi) - digamma(ak) + np.log(bk))
-			-(ak/(2.*bk)) * (x - nu_k)**2
-			-(ak/(2.*bk)) * gammaf(3./2.) * (2*np.pi)**(-D/2.) * 2.**(3./2.)
-		) for (ak, bk, nu_k, zeta_k) in zip(a, b, nu, zeta))
+bound = sum( #over k
+			zeta_k * (
+				-D/2. * (np.log(2 * np.pi) - digamma(ak) + np.log(bk))
+				-(ak/(2.*bk)) * ((x[None, :] - nu_k)**2).sum(axis=1)
+				-(ak/(2.*bk)) * D * gammaf(3./2.) * (2*np.pi)**(-1/2.) * 2.**(3./2.) / omega_k
+			)
+		for (ak, bk, nu_k, zeta_k, omega_k) in zip(a, b, nu, zeta, omega))
 print("Analytical x term in ELBO:", bound)
 
 # Monte Carlo
@@ -140,5 +165,5 @@ for i in range(N):
 	tau = q_tau.rvs()
 	z = q_z.rvs()
 
-	bounds.append(p_x(z, mu, tau).logpdf(x)) #There's only a single datapoint, so no need for sum
+	bounds.append(sum(p_x(z, mu, tau).logpdf(x))) #sum over d (There's only a single datapoint, so no need for sum over i)
 	if i%5000 == 0: print(i, np.mean(bounds))

spherical/demos.py
@@ -26,8 +26,8 @@ def gen_demo_data(batch_size=1, np_seed=None, D=2, use_zeros=True):
         K = np.random.randint(2, high=6+1)
         in_dataset = 0
         for k in range(K):
-            mean = 4*np.random.randn(D) - 1
-            cov = 0.1*np.eye(D) + np.random.rand()*np.eye(D)
+            mean = 3.5*np.random.randn(D) - 1
+            cov = 0.1*np.eye(D) + [0.25,1][np.random.randint(2)]*np.eye(D)
             n = np.random.randint(10, high=25+1)
             in_dataset += n
             gaussian_data = np.random.multivariate_normal(mean, cov, n)

spherical/dpgmm_vi.py
@@ -4,13 +4,13 @@ import numpy as np
 import collections
 
 """
-SPHERICAL COVARIANCE ie. isotropic
+Spherical COVARIANCE
 
 Unsupervised clustering in R^D
 
 TF implementation of variational inference in a 
 Dirichlet Process isotropic Gaussian mixture model
-Derivation in Variational_Inference_in_DPGMM_derivation_Isotropic.pdf included in git repo 
+Derivation in Variational_Inference_in_DPGMM_derivation.pdf included in git repo 
 
 Clusters matrix X (batch_size x N x D) of N datapoints with dimensionality D
 If a datapoint = zero vector, it is ignored. 
@@ -26,14 +26,13 @@ class dpgmm():
 
     ######### INITIALIZATION ##########################################################################################################
 
-    def __init__(self, alpha, D, n_iter, T, covariance_type='isotropic'):
+    def __init__(self, alpha, D, n_iter, T, covariance_type='diagonal'):
 
         self.alpha = alpha; #Dirichlet concentration parameter
         self.D = D; self.Dfl = tf.cast(self.D, dtype=tf.float32); #dimensionality of data
         self.T = T; #truncation value
-        
-        #constants
-        self.gaussian_integral = np.divide( gamma_func(1.5), (0.5**1.5)*((2.*np.pi)**(D/2.)) )
+ 
+        self.gaussian_const = np.divide(gamma_func(1.5)*(2.0**1.5)*self.D, np.sqrt(2.*np.pi))
 
         #Initialization settings
         self.mu_std = 5.
@@ -62,6 +61,8 @@ class dpgmm():
                 initializer=tf.ones_initializer())
         nu = tf.get_variable("nu", shape_TD, dtype=tf.float32,
                 initializer=tf.random_normal_initializer(stddev=self.mu_std)) 
+        omega = tf.get_variable("omega", shape_T, dtype=tf.float32,
+                initializer=tf.ones_initializer())
 
         if shared:
 
@@ -70,6 +71,7 @@ class dpgmm():
             lambda_1 = tf.tile(lambda_1[tf.newaxis, :], [batch_size, 1])
             lambda_2 = tf.tile(lambda_2[tf.newaxis, :], [batch_size, 1])
             nu = tf.tile(nu[tf.newaxis, :, :], [batch_size, 1, 1])
+            omega = tf.tile(omega[tf.newaxis, :], [batch_size, 1])
         
         # zeta will be the first in the update distribution 
         # so this initialization is only necessary for ELBO calculation
@@ -83,7 +85,7 @@ class dpgmm():
         else:
             mask = tf.ones([batch_size, N])
 
-        return a, b, lambda_1, lambda_2, nu, zeta, mask
+        return a, b, lambda_1, lambda_2, nu, omega, zeta, mask
 
     ######### UPDATE EQUATIONS ##########################################################################################################
 
@@ -102,41 +104,65 @@ class dpgmm():
         # nu_z batch N T
         # a batch newaxis T
         # b batch newaxis T
-        w = tf.divide(tf.multiply(zeta_mask, a[:, tf.newaxis, :]), b[:, tf.newaxis,:]) 
-        #w : batch N T newaxis
+        w = tf.divide(tf.multiply(zeta_mask, 
+            a[:, tf.newaxis, :]), b[:, tf.newaxis,:])[:, :, :, tf.newaxis] 
+        #w : batch N T newaxis 
         #X : batch N newaxis D
-        numer = tf.reduce_sum(tf.multiply(w[:, :, :, tf.newaxis], X[:, :, tf.newaxis, :]), axis=1) #over N
+        numer = tf.reduce_sum(tf.multiply(w, X[:, :, tf.newaxis, :]), axis=1) #over N
         denom = 1.0 + tf.reduce_sum(w, axis=1) #over N
         # numer batch T D
-        # denom batch T newaxis
-        nu = tf.divide(numer,  denom[:, :, tf.newaxis])
+        # denom batch T D
+        nu = tf.divide(numer,  denom)
         return nu
 
-    def update_ab(self, nu, zeta_mask, X):
+    def update_omega(self, a, b, zeta_mask):
+
+        # a batch newaxis T
+        # b batch newaxis T
+        ratio = tf.multiply(tf.divide(a, b), self.gaussian_const)[:, tf.newaxis, :]
+        # nu_z batch N T 
+        #WANT: omega batch T
+        omega = 1.0 + tf.reduce_sum( tf.multiply(zeta_mask, ratio) , axis=1) #over N
+        return omega
+
+    def update_ab(self, nu, omega, zeta_mask, X):
         #nu_z_masked batch N T
+        #a batch T
         a = 1.0 + tf.multiply(self.Dfl/2.0, tf.reduce_sum(zeta_mask, axis=1))#over N
+        
         #X batch N newaxis D
-        #nu_mu batch newaxis T D
-        difference = X[:,:,tf.newaxis,:] - nu[:,tf.newaxis,:,:]
-        sos = tf.reduce_sum(tf.square(difference), axis=3)
-        b = 1.0 + 0.5*tf.reduce_sum(tf.multiply(zeta_mask, sos + self.gaussian_integral), axis=1)
+        #nu batch newaxis T D
+        #difference norm batch N T        
+        difference_norm = tf.reduce_sum(tf.square(X[:,:,tf.newaxis,:] - nu[:,tf.newaxis,:,:]),axis=3)
+        #omega batch newaxis T
+        s = difference_norm + tf.divide(self.gaussian_const, omega[:, tf.newaxis, :])
+        #s batch N T 
+        #zeta_mask batch N T
+        #b batch T 
+        b = 1.0 + 0.5*tf.reduce_sum(tf.multiply(zeta_mask, s), axis=1) #over N
+        
         return a, b
 
-    def eta_x(self, a, b, nu, X):
+    def eta_x(self, a, b, nu, omega, X):
         """
-        eta_x = E_q[log P(x_i | z_i = k, mu, var)]
+        eta_x_{i, k} = E_q[log P(x_{i} | z_i = k, mu_{k}, var_{k})]
+
         """
 
-        #ab terms: batch_size, T
-        ab1 = tf.multiply(-self.Dfl/2.0, tf.log(2*np.pi) - tf.digamma(a) + tf.log(b))
-        ab2 = tf.divide(a, -2.0*b)
+        #a batch_size, T
+        #b batch_size, T
+        ab1 = tf.multiply(-self.Dfl/2.0, tf.log(2*np.pi) - tf.digamma(a) + tf.log(b))[:,tf.newaxis, :]
+        ab2 = tf.divide(a, -2.0*b)[:,tf.newaxis, :]
 
         # X: batch N newaxis D
-        # mu: batch newaxis T D 
-        difference = tf.subtract(X[:, :, tf.newaxis, :], nu[:, tf.newaxis, :, :]) 
-        #sum over D --> sum_of_squares: batch_size N T
-        sos = tf.reduce_sum(tf.square(difference),axis=3)
-        Eq = ab1[:, tf.newaxis, :] + tf.multiply(ab2[:,tf.newaxis, :], sos + self.gaussian_integral)
+        # mu: batch newaxis T D
+        norm_difference = tf.reduce_sum(tf.square(tf.subtract(X[:, :, tf.newaxis, :], nu[:, tf.newaxis, :, :])),axis=3)
+        #omega: batch newaxis T 
+        s = norm_difference + tf.divide(self.gaussian_const, omega[:, tf.newaxis, :])
+        #s: batch N T
+        #ab1 batch_size, newaxis, T,
+        #ab2 batch_size, newaxis, T,
+        Eq = ab1 + tf.multiply(ab2, s)
 
         return Eq
 
@@ -147,9 +173,12 @@ class dpgmm():
         d_cumsum = tf.cumsum(d2, axis=1, exclusive=True)
         return d1 + d_cumsum
 
-    def update_zeta(self, a, b, lambda_1, lambda_2, nu, X): 
+    def update_zeta(self, a, b, lambda_1, lambda_2, nu, omega, X): 
 
-        prop_log_zeta = self.eta_z(lambda_1, lambda_2)[:, tf.newaxis, :] - 1. + self.eta_x(a, b, nu, X)
+        #self.eta_x: batch N T 
+        #self.eta_z: batch newaxis T
+
+        prop_log_zeta = self.eta_z(lambda_1, lambda_2)[:, tf.newaxis, :] - 1. + self.eta_x(a, b, nu, omega, X) 
         #prop_log_nu_z batch N T 
         log_zeta = tf.subtract(prop_log_zeta, tf.reduce_logsumexp(prop_log_zeta, axis=2, keepdims=True)) #over T
         zeta = tf.exp(log_zeta)
@@ -158,13 +187,15 @@ class dpgmm():
 
     def update_all(self, L, dataset):
 
-        zeta = self.update_zeta(L.a, L.b, L.lambda_1, L.lambda_2, L.nu, dataset.X)
+
+        zeta = self.update_zeta(L.a, L.b, L.lambda_1, L.lambda_2, L.nu, L.omega, dataset.X)
         zeta_mask = tf.multiply(dataset.mask[:,:,tf.newaxis],zeta)
         lambda_1, lambda_2 = self.update_lambda(zeta_mask)
         nu = self.update_nu(L.a, L.b, zeta_mask, dataset.X)
-        a, b = self.update_ab(nu, zeta_mask, dataset.X)
+        omega = self.update_omega(L.a, L.b, zeta_mask)
+        a, b = self.update_ab(nu, omega, zeta_mask, dataset.X) #might have to mess with the order of these
 
-        return a, b, lambda_1, lambda_2, nu, zeta
+        return a, b, lambda_1, lambda_2, nu, omega, zeta
 
     ######### VARIATIONAL LOWER BOUND ##########################################################################################################
 
@@ -182,16 +213,17 @@ class dpgmm():
         vb = tf.reduce_sum(term1 + term2 + term3 - term4 - term5, axis=1)
         return vb
 
-    def mu_lower_bound_term(self, nu):
-        #nu_mu: [batch_size, T, D]
-        tot = -0.5 * tf.reduce_sum(tf.square(nu), axis=2)
-        vb = tf.reduce_sum(tot, axis=1)
+    def mu_lower_bound_term(self, nu, omega):
+        #nu: [batch_size, T, D]
+        #omega [batch_size, T]
+        tot = tf.multiply(-self.Dfl/2.0, tf.divide(1.0, omega) + tf.log(omega) - 1.0) - 0.5*tf.reduce_sum(tf.square(nu),axis=2)
+        vb = tf.reduce_sum(tot, axis=1) #over number of clusters
         return vb
 
     def tau_lower_bound_term(self, a, b):
         #a, b: [batch_size, T]
         tot = tf.lgamma(a) - tf.multiply(a - 1.,tf.digamma(a)) - tf.log(b) + a - tf.divide(a, b)
-        vb = tf.reduce_sum(tot, axis=1)
+        vb = tf.reduce_sum(tot, axis=1) #sum over clusters 
         return vb
 
     def z_lower_bound_term(self, lambda_1, lambda_2, zeta, mask):
@@ -207,11 +239,11 @@ class dpgmm():
 
         return vb
 
-    def x_lower_bound_term(self, a, b, nu, zeta_mask, X):
-        #X: batch_size, nDatapoints, D
-        #E_q[log P(x_i | z_i = k, mu, var)]
-        #c terms are all dimension: batch T
-        EqLogPxGivenZ = self.eta_x(a, b, nu, X)
+    def x_lower_bound_term(self, a, b, nu, omega, zeta_mask, X):
+        #X: batch_size, N, D
+        #self.eta_x: batch N T 
+        EqLogPxGivenZ = self.eta_x(a, b, nu, omega, X)
+        #zeta_mask: batch_size, N, T
         tot = tf.multiply(zeta_mask, EqLogPxGivenZ)
         #tot batch_size N T
         vb = tf.reduce_sum(tot, axis=[1,2])
@@ -219,15 +251,15 @@ class dpgmm():
 
     def evidence_lower_bound(self, L, D):
         phi_lb = self.phi_lower_bound_term(L.lambda_1, L.lambda_2) 
-        mu_lb = self.mu_lower_bound_term(L.nu) 
+        mu_lb = self.mu_lower_bound_term(L.nu, L.omega) 
         tau_lb = self.tau_lower_bound_term(L.a, L.b)
         z_lb = self.z_lower_bound_term(L.lambda_1, L.lambda_2, L.zeta, D.mask)
-        x_lb = self.x_lower_bound_term(L.a, L.b, L.nu, tf.multiply(D.mask[:,:,tf.newaxis],L.zeta), D.X)
+        x_lb = self.x_lower_bound_term(L.a, L.b, L.nu, L.omega, tf.multiply(D.mask[:,:,tf.newaxis],L.zeta), D.X)
         return phi_lb + mu_lb + tau_lb + z_lb + x_lb
 
     ######### INFERENCE FUNCTIONS ##########################################################################################################
 
-    def infer(self, _a, _b, _lambda_1, _lambda_2, _nu, _zeta, X, mask):
+    def infer(self, _a, _b, _lambda_1, _lambda_2, _nu, _omega, _zeta, X, mask):
         """
         Performs variational inference in DPGMM for n_iter number of iterations,
         then returns inferred latent variables
@@ -239,20 +271,20 @@ class dpgmm():
             
         ##Initial input into "while" loop, i.e., inference iterations
         i = tf.constant(0)
-        latents = collections.namedtuple('latents', ['a', 'b', 'lambda_1', 'lambda_2', 'nu', 'zeta'])
+        latents = collections.namedtuple('latents', ['a', 'b', 'lambda_1', 'lambda_2', 'nu', 'omega', 'zeta'])
         dataset = collections.namedtuple('dataset', ['X', 'mask'])
-        init_iteration = (i, latents(_a, _b, _lambda_1, _lambda_2, _nu, _zeta), dataset(X, mask))
+        init_iteration = (i, latents(_a, _b, _lambda_1, _lambda_2, _nu, _omega, _zeta), dataset(X, mask))
 
         cond = lambda i, L, D: i < self.n_iter
         def body(i, L, D):
-            a, b, lambda_1, lambda_2, nu, zeta = self.update_all(L, D)              
-            return (i + 1, latents(a, b, lambda_1, lambda_2, nu, zeta), D)
+            a, b, lambda_1, lambda_2, nu, omega, zeta = self.update_all(L, D)              
+            return (i + 1, latents(a, b, lambda_1, lambda_2, nu, omega, zeta), D)
         
         final_iteration = tf.while_loop(cond, body, init_iteration)
 
         return final_iteration[1]   
 
-    def elbo_infer(self, _a, _b, _lambda_1, _lambda_2, _nu, _zeta, X, mask, batch_size):
+    def elbo_infer(self, _a, _b, _lambda_1, _lambda_2, _nu, _omega, _zeta, X, mask, batch_size):
         """
         Performs variational inference in DPGMM for n_iter number of iterations,
         and also calculates the change in ELBO at each update
@@ -260,41 +292,45 @@ class dpgmm():
         """
 
         i = tf.constant(0)
-        latents = collections.namedtuple('latents', ['a', 'b', 'lambda_1', 'lambda_2', 'nu', 'zeta'])
+        latents = collections.namedtuple('latents', ['a', 'b', 'lambda_1', 'lambda_2', 'nu', 'omega', 'zeta'])
         dataset = collections.namedtuple('dataset', ['X', 'mask'])
         #ELBO term names: "updated-variable_term-of-lower-bound"
-        ELBO_terms = ['zeta_z', 'zeta_x', 'lambda_phi', 'lambda_z', 'nu_mu', 'nu_x', 'ab_tau', 'ab_x', 'total']
-        empty_ELBO_terms = tuple([tf.TensorArray(dtype=tf.float32, size=self.n_iter, element_shape=batch_size, name=ELBO_terms[j]) for j in range(9)])
-        init_iteration = (i, latents(_a, _b, _lambda_1, _lambda_2, _nu, _zeta), dataset(X, mask)) + empty_ELBO_terms
+        ELBO_terms = ['zeta_z', 'zeta_x', 'lambda_phi', 'lambda_z', 'nu_mu', 'nu_x', 'omega_mu', 'omega_x', 'ab_tau', 'ab_x', 'total']
+        empty_ELBO_terms = tuple([tf.TensorArray(dtype=tf.float32, size=self.n_iter, element_shape=batch_size, name=ELBO_terms[j]) for j in range(11)])
+        init_iteration = (i, latents(_a, _b, _lambda_1, _lambda_2, _nu, _omega, _zeta), dataset(X, mask)) + empty_ELBO_terms
 
-        cond = lambda i, L, D, zeta_z, zeta_x, lambda_phi, lambda_z, nu_mu, nu_x, ab_tau, ab_x, total: i < self.n_iter 
-        def body(i, L, D, zeta_z, zeta_x, lambda_phi, lambda_z, nu_mu, nu_x, ab_tau, ab_x, total):
+        cond = lambda i, L, D, zeta_z, zeta_x, lambda_phi, lambda_z, nu_mu, nu_x, omega_mu, omega_x, ab_tau, ab_x, total: i < self.n_iter 
+        def body(i, L, D, zeta_z, zeta_x, lambda_phi, lambda_z, nu_mu, nu_x, omega_mu, omega_x, ab_tau, ab_x, total):
 
-            zeta = self.update_zeta(L.a, L.b, L.lambda_1, L.lambda_2, L.nu, D.X)
+            zeta = self.update_zeta(L.a, L.b, L.lambda_1, L.lambda_2, L.nu, L.omega, D.X)
             zeta_mask = tf.multiply(D.mask[:,:,tf.newaxis], zeta)
             zeta_z = zeta_z.write(i, self.z_lower_bound_term(L.lambda_1, L.lambda_2, zeta, D.mask)-self.z_lower_bound_term(L.lambda_1, L.lambda_2, L.zeta, D.mask))
-            zeta_x = zeta_x.write(i, self.x_lower_bound_term(L.a, L.b, L.nu, zeta_mask, D.X)-self.x_lower_bound_term(L.a, L.b, L.nu, tf.multiply(D.mask[:,:,tf.newaxis],L.zeta), D.X))
+            zeta_x = zeta_x.write(i, self.x_lower_bound_term(L.a, L.b, L.nu, L.omega, zeta_mask, D.X)-self.x_lower_bound_term(L.a, L.b, L.nu, L.omega, tf.multiply(D.mask[:,:,tf.newaxis],L.zeta), D.X))
 
             lambda_1, lambda_2 = self.update_lambda(zeta_mask)
             lambda_phi = lambda_phi.write(i, self.phi_lower_bound_term(lambda_1, lambda_2) - self.phi_lower_bound_term(L.lambda_1, L.lambda_2))
             lambda_z = lambda_z.write(i,self.z_lower_bound_term(lambda_1, lambda_2, zeta, D.mask)-self.z_lower_bound_term(L.lambda_1, L.lambda_2, zeta, D.mask))
 
             nu = self.update_nu(L.a, L.b, zeta_mask, D.X)
-            nu_mu = nu_mu.write(i, self.mu_lower_bound_term(nu)-self.mu_lower_bound_term(L.nu))
-            nu_x = nu_x.write(i, self.x_lower_bound_term(L.a, L.b, nu, zeta_mask, D.X)-self.x_lower_bound_term(L.a, L.b, L.nu, zeta_mask, D.X))
+            nu_mu = nu_mu.write(i, self.mu_lower_bound_term(nu, L.omega)-self.mu_lower_bound_term(L.nu, L.omega))
+            nu_x = nu_x.write(i, self.x_lower_bound_term(L.a, L.b, nu, L.omega, zeta_mask, D.X)-self.x_lower_bound_term(L.a, L.b, L.nu, L.omega, zeta_mask, D.X))
 
-            a, b = self.update_ab(nu, zeta_mask, D.X)
+            omega = self.update_omega(L.a, L.b, zeta_mask)
+            omega_mu = omega_mu.write(i, self.mu_lower_bound_term(nu, omega)-self.mu_lower_bound_term(nu, L.omega))
+            omega_x = omega_x.write(i, self.x_lower_bound_term(L.a, L.b, nu, omega, zeta_mask, D.X)-self.x_lower_bound_term(L.a, L.b, nu, L.omega, zeta_mask, D.X))            
+
+            a, b = self.update_ab(nu, omega, zeta_mask, D.X)
             ab_tau = ab_tau.write(i, self.tau_lower_bound_term(a, b) - self.tau_lower_bound_term(L.a, L.b))
-            ab_x = ab_x.write(i, self.x_lower_bound_term(a, b, nu, zeta_mask, D.X)-self.x_lower_bound_term(L.a, L.b, nu, zeta_mask, D.X))
+            ab_x = ab_x.write(i, self.x_lower_bound_term(a, b, nu, omega, zeta_mask, D.X)-self.x_lower_bound_term(L.a, L.b, nu, omega, zeta_mask, D.X))
 
-            updated_L = latents(a, b, lambda_1, lambda_2, nu, zeta)
+            updated_L = latents(a, b, lambda_1, lambda_2, nu, omega, zeta)
             total = total.write(i, self.evidence_lower_bound(updated_L, D) - self.evidence_lower_bound(L, D))
 
-            return (i+1, updated_L, D, zeta_z, zeta_x, lambda_phi, lambda_z, nu_mu, nu_x, ab_tau, ab_x, total)           
+            return (i+1, updated_L, D, zeta_z, zeta_x, lambda_phi, lambda_z, nu_mu, nu_x, omega_mu, omega_x, ab_tau, ab_x, total)           
 
         final_iteration = tf.while_loop(cond, body, init_iteration)
 
-        return final_iteration[1], [final_iteration[i].stack() for i in range(3,12)]
+        return final_iteration[1], [final_iteration[i].stack() for i in range(3,14)]
 
 def variational_inference(data, alpha=1.0, T=10, n_iter=10, tf_seed=None, get_elbo=False, tf_device='/cpu:0'):
     """
@@ -321,12 +357,12 @@ def variational_inference(data, alpha=1.0, T=10, n_iter=10, tf_seed=None, get_el
             X = tf.placeholder(tf.float32, shape=[batch_size, N, D])            
 
             mixture_model = dpgmm(alpha, D, n_iter, T)
-            init_a, init_b, init_lambda_1, init_lambda_2, init_nu, init_zeta, mask = mixture_model.initialize_latents(X, batch_size, shared=False)
+            init_a, init_b, init_lambda_1, init_lambda_2, init_nu, init_omega, init_zeta, mask = mixture_model.initialize_latents(X, batch_size, shared=False)
 
             if not get_elbo:
-                inferred_latents = mixture_model.infer(init_a, init_b, init_lambda_1, init_lambda_2, init_nu, init_zeta, X, mask)
+                inferred_latents = mixture_model.infer(init_a, init_b, init_lambda_1, init_lambda_2, init_nu, init_omega, init_zeta, X, mask)
             else:
-                inferred_latents, ELBO_deltas = mixture_model.elbo_infer(init_a, init_b, init_lambda_1, init_lambda_2, init_nu, init_zeta, X, mask, batch_size)
+                inferred_latents, ELBO_deltas = mixture_model.elbo_infer(init_a, init_b, init_lambda_1, init_lambda_2, init_nu, init_omega, init_zeta, X, mask, batch_size)
 
         ##Run graph
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)) as sess:
@@ -337,5 +373,5 @@ def variational_inference(data, alpha=1.0, T=10, n_iter=10, tf_seed=None, get_el
                 return inferred_latents_out[0]
             else:
                 inferred_latents_out, ELBO_deltas_out = sess.run([inferred_latents, ELBO_deltas], feed_dict = {X: data})
-                ELBO_terms = collections.namedtuple('ELBO_terms', ['zeta_z', 'zeta_x', 'lambda_phi', 'lambda_z', 'nu_mu', 'nu_x', 'ab_tau', 'ab_x', 'total'])
+                ELBO_terms = collections.namedtuple('ELBO_terms', ['zeta_z', 'zeta_x', 'lambda_phi', 'lambda_z', 'nu_mu', 'nu_x', 'omega_mu', 'omega_x', 'ab_tau', 'ab_x', 'total'])
                 return inferred_latents_out, ELBO_terms(*ELBO_deltas_out)